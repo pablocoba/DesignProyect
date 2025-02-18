3f551f4d7f1b8472977a121e39f9a1aab5a6fafa
pablocoba <contact.p4ul@gmail.com>
2025-02-18 11:52:25 +0100

    18/02
    otros cambios de accesibilidad, animaciones, sprites

--- accesibilidad.js
+++ accesibilidad.js
@@ -1,7 +1,39 @@
+let accesibilidad = document.getElementById("accesibilidadBtn")
 let fontSize = document.getElementById("fontSize")
+let accWdw = document.getElementById("accesibilidadWdw")
+let animation = document.getElementById("animationOnOff")
+
+accesibilidad.addEventListener("click", event=>{
+    if(accWdw.style.display == "block"){
+        accWdw.style.display = "none"
+        console.log(accWdw.style.display)
+    }
+    else{
+        accWdw.style.display = "block"
+        console.log(accWdw.style.display)
+    }
+})
+//hace que las casillas estén desmarcadas por defecto
 document.addEventListener("DOMContentLoaded",event=>{
     fontSize.checked = false;
+    animation.checked = false;
 })
+
+let animationOnOff = document.getElementById('animationOnOff');
+
+animationOnOff.addEventListener('change', event => {
+  let elementosAnimados = document.getElementsByClassName('borde-animado')
+  for(e of elementosAnimados){
+    if(e.style.animation != "none"){
+        e.style.animation = "none"
+    }
+    else{
+        e.style.animation = "border-color-change 5s ease infinite"
+    }
+  }
+  
+});
+
 fontSize.addEventListener("change", event=>{
     const elementos = document.querySelectorAll("*")
     if(event.target.checked){
